bb44f7fb9a86e4702a78f4599a85444ebf229ccc
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-07-12 22:40:19 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/ldszmeta/LdszmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ldszmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdszmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdszmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/ldszmeta/LdszmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ldszmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdszmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
