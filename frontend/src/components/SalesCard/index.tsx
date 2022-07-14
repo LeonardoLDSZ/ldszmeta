282132e00b76e3e73a0ac28dbc59d237d66b0959
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-07-14 01:09:11 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,80 +1,90 @@
-import NotificationButton from '../NotificationButton';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
 
-import './styles.css'
+import "./styles.css";
 
 function SalesCard() {
   return (
     <div className="ldszmeta-card">
-            <h2 className="ldszmeta-sales-title">Sales</h2>
-            <div>
-              <div className="ldszmeta-form-control-container">
-                <input className="ldszmeta-form-control" type="text"/>
-              </div>
-              <div className="ldszmeta-form-control-container">
-                <input className="ldszmeta-form-control" type="text"/>
-              </div>
-            </div>
+      <h2 className="ldszmeta-sales-title">Sales</h2>
+      <div>
+        <div className="ldszmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="ldszmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
 
-            <div>
-              <table className="ldszmeta-sales-table">
-                <thead>
-                  <tr>
-                    <th className="show992">ID</th>
-                    <th className="show576">Data</th>
-                    <th>Seller</th>
-                    <th className="show992">Visits</th>
-                    <th className="show992">Sales</th>
-                    <th>Total</th>
-                    <th>Notify</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="ldszmeta-red-btn-container">
-                        <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="ldszmeta-red-btn-container">
-                      <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="ldszmeta-red-btn-container">
-                      <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-
-              </table>
-            </div>
-
-          </div>
-  )
+      <div>
+        <table className="ldszmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Seller</th>
+              <th className="show992">Visits</th>
+              <th className="show992">Sales</th>
+              <th>Total</th>
+              <th>Notify</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="ldszmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="ldszmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="ldszmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default SalesCard;
