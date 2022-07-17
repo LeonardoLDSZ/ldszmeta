6d4a75010cb19fffbd6ddd6ff3c6a67c08f5ba01
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-07-17 09:57:17 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/ldszmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.ldszmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.ldszmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
